94fa387d0be9b09c501b13e18cc85e2927facbd5
Michael Sparre <67245304+micsparre@users.noreply.github.com>
2023-09-22 11:54:55 -0700

    fixed path comparisons of photos (was comparing list of length 1 to string)

--- wallpaper/main.py
+++ wallpaper/main.py
@@ -1,13 +1,16 @@
+import random
+import appscript
 import os
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
-import appscript
-import random
+
 
 def get_random_file(folder_path):
-    files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+    files = [f for f in os.listdir(folder_path) if os.path.isfile(
+        os.path.join(folder_path, f))]
     if not files:
         return None
     return os.path.join(folder_path, random.choice(files))
+
 
 horizontal_folder_path = os.path.abspath("../horizontal_images/")
 vertical_folder_path = os.path.abspath("../vertical_images/")
@@ -18,18 +21,17 @@ monitor = None
 for d in desktops:
     img_path = None
     desk = se.desktops[appscript.its.display_name == d]
-    curr_img_path = desk.picture.get()
-    
+    curr_img_path = desk.picture.get()[0]
+
     if d == 'S27E590':
         folder = horizontal_folder_path
         monitor = 'horizontal'
     elif d == 'Kg251Q':
         folder = vertical_folder_path
         monitor = 'vertical'
-    
     while True:
-            img_path = get_random_file(folder)
-            if img_path != curr_img_path:
-                break
+        img_path = get_random_file(folder)
+        if img_path != curr_img_path:
+            break
     print(f'Setting {monitor} monitor wallpaper to {img_path}')
     desk.picture.set(appscript.mactypes.File(img_path))
